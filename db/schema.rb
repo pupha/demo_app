f95e0477d89eade64c1550c56b204737286902fe
pupha <redhouse2005@gmail.com>
2013-10-17 15:18:24 +0900

    Add Micropost model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017055422) do
+ActiveRecord::Schema.define(version: 20131017061014) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
